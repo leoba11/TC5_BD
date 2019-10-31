7912efef04b1d040f24c30c831517b7ccf929fdb
leoba11 <leobarri.2013@gmail.com>
2019-10-30 23:44:50 -0600

    cambiosssss

--- View/Contactos/delete.php
+++ View/Contactos/delete.php
@@ -6,10 +6,10 @@
 <h1 style="text-align:center;">¿Está seguro que desea eliminar este contacto?</h1>
 
 <p style="text-align:center;" >
-	<button onclick="window.location.href = '/~eb50946/TC5/?action=index';">Cancelar</button> 
+	<button onclick="window.location.href = '/~eb50946/TC5_BD/?action=index';">Cancelar</button> 
 </p>
 
-<form name="sure" id="sure" method="post" action="/~eb50946/TC5/?id=<?php echo $contact->id?>">
+<form name="sure" id="sure" method="post" action="/~eb50946/TC5_BD/?id=<?php echo $contact->id?>">
 
   <input name="action" type="hidden" value="sure" />
   
