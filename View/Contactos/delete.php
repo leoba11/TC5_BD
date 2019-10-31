b1fd85ddaf1174eff9158aa32e284382c3cf49dd
leoba11 <leobarri.2013@gmail.com>
2019-10-30 23:40:39 -0600

    cambiosssss

--- View/Contactos/delete.php
+++ View/Contactos/delete.php
@@ -6,20 +6,20 @@
 <h1 style="text-align:center;">¿Está seguro que desea eliminar este contacto?</h1>
 
 <p style="text-align:center;" >
-	<button onclick="window.location.href = '/~eb70442/TC5/?action=index';">Cancelar</button> 
+	<button onclick="window.location.href = '/~eb50946/TC5/?action=index';">Cancelar</button> 
 </p>
 
-<form name="sure" id="sure" method="post" action="/~eb70442/TC5/?id=<?php echo $contact->id?>">
+<form name="sure" id="sure" method="post" action="/~eb50946/TC5/?id=<?php echo $contact->id?>">
 
   <input name="action" type="hidden" value="sure" />
   
-  <p style="text-align:center;">Nombre: <u> <?php echo $contact->nombre?> </u> </p>
-  <p style="text-align:center;">Apellido: <u><?php echo $contact->apellido?> </u></p>
-  <p style="text-align:center;">Telefono de casa: <u><?php echo $contact->telefono_casa?></u></p>
-  <p style="text-align:center;">Direccion de casa: <u><?php echo $contact->direccion_casa?></u></p>
-  <p style="text-align:center;">Telefono de trabajo: <u><?php echo $contact->telefono_trabajo?></u></p>
-  <p style="text-align:center;">Direccion de trabajo: <u><?php echo $contact->direccion_trabajo?></u></p>
-  <p style="text-align:center;">Email: <u><?php echo $contact->correo?></u></p>
+  <p style="text-align:center;">Nombre: <u> <?php echo $contact->name?> </u> </p>
+  <p style="text-align:center;">Apellido: <u><?php echo $contact->last_name?> </u></p>
+  <p style="text-align:center;">Telefono de casa: <u><?php echo $contact->home_telephone?></u></p>
+  <p style="text-align:center;">Direccion de casa: <u><?php echo $contact->home_address?></u></p>
+  <p style="text-align:center;">Telefono de trabajo: <u><?php echo $contact->work_telephone?></u></p>
+  <p style="text-align:center;">Direccion de trabajo: <u><?php echo $contact->work_address?></u></p>
+  <p style="text-align:center;">Email: <u><?php echo $contact->email?></u></p>
 
     </br>
 	<div style="text-align:center;">
